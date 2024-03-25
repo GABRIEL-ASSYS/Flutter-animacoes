bdceb789dcf78a1ec571b7c6667346d30ad3740b
Gabriel Assys <engs-gabrieloliveira@camporeal.edu.br>
2024-03-25 10:10:12 -0300

    adicionando animacao a SplashScreen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:grimorio/screens/dashboard.dart';
 import 'package:grimorio/theme.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -9,6 +11,15 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double imageOpacity = 0;
+  int timeToAnimationInMilliseconds = 2000;
+
+  @override
+  void initState() {
+    waitAndShow();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,9 +34,41 @@ class _SplashScreenState extends State<SplashScreen> {
                 width: 300,
               ),
             ),
+            AnimatedOpacity(
+              opacity: imageOpacity,
+              duration: Duration(milliseconds: timeToAnimationInMilliseconds),
+              curve: Curves.ease,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Image.asset(
+                  'assets/images/grimorio_titulo_1152.png',
+                  width: 300,
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  waitAndShow() {
+    Future.delayed(const Duration(milliseconds: 700)).then((value) {
+      setState(() {
+        imageOpacity = 1;
+      });
+      waitAndNextScreen();
+    });
+  }
+
+  waitAndNextScreen() {
+    Future.delayed(Duration(milliseconds: timeToAnimationInMilliseconds + 300))
+        .then((value) {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Dashboard(),
+          ));
+    });
   }
 }
